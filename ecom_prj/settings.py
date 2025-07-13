ffc799364ca189b22f5085236cbd65a942c92e35
Venkatesh Joshi <venkateshrj12@gmail.com>
2025-07-13 14:05:52 +0530

    create different apps

--- ecom_prj/settings.py
+++ ecom_prj/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'userauths',
+    'store',
+    'vendor',
+    'customer',
+    'blog',
 ]
 
 MIDDLEWARE = [
